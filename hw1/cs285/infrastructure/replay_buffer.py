d67e915cf224f2ca312035a8253bdc0d87c9c67c
Haoran Liao <haoran@physics.mcgill.ca>
2020-09-09 18:50:09 -0700

    update

--- hw1/cs285/infrastructure/replay_buffer.py
+++ hw1/cs285/infrastructure/replay_buffer.py
@@ -76,7 +76,7 @@ class ReplayBuffer(object):
         ## HINT 1: use np.random.permutation to sample random indices
         ## HINT 2: return corresponding data points from each array (i.e., not different indices from each array)
         ## HINT 3: look at the sample_recent_data function below
-        rand_inds = np.random.permutation(range(len(self.obs)))[batch_size:batch_size*2]
+        rand_inds = np.random.permutation(range(len(self.obs)))[:batch_size]
         return self.obs[rand_inds], self.acs[rand_inds], self.rews[rand_inds], self.next_obs[rand_inds], self.terminals[rand_inds] 
         # return TODO, TODO, TODO, TODO, TODO
 
