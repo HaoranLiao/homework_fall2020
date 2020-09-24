bfb04e50c432334d06568bea8e5766e1b1d07f9f
Haoran Liao <haoran@physics.mcgill.ca>
2020-09-24 13:36:05 -0700

    Update plot.py

--- hw2/plot.py
+++ hw2/plot.py
@@ -24,6 +24,11 @@ curve_q4_7 = np.load("/Users/haoran/Documents/GitHub/homework_fall2020/hw2/data/
 curve_q4_8 = np.load("/Users/haoran/Documents/GitHub/homework_fall2020/hw2/data/q4_search_b50000_lr0.01_rtg_nnbaseline.npy")
 curve_q4_9 = np.load("/Users/haoran/Documents/GitHub/homework_fall2020/hw2/data/q4_search_b50000_lr0.02_rtg_nnbaseline.npy")
 
+curve_q4_run_1 = np.load("/Users/haoran/Documents/GitHub/homework_fall2020/hw2/data/q4_b30000_r0.02.npy")
+curve_q4_run_2 = np.load("/Users/haoran/Documents/GitHub/homework_fall2020/hw2/data/q4_b30000_r0.02_rtg.npy")
+curve_q4_run_3 = np.load("/Users/haoran/Documents/GitHub/homework_fall2020/hw2/data/q4_b30000_r0.02_nnbaseline.npy")
+curve_q4_run_4 = np.load("/Users/haoran/Documents/GitHub/homework_fall2020/hw2/data/q4_b30000_r0.02_rtg_nn_baseline.npy")
+
 from matplotlib import pyplot as plt
 
 ################## q1 #################
@@ -59,17 +64,26 @@ from matplotlib import pyplot as plt
 # plt.show()
 
 
-################# q4 ##################
+################# q4 search ##################
+# plt.figure()
+# plt.plot(curve_q4_1, label='b10000_lr0.005')
+# plt.plot(curve_q4_2, label='b10000_lr0.01')
+# plt.plot(curve_q4_3, label='b10000_lr0.02')
+# plt.plot(curve_q4_4, label='b30000_lr0.005')
+# plt.plot(curve_q4_5, label='b30000_lr0.01')
+# plt.plot(curve_q4_6, label='b30000_lr0.02')
+# plt.plot(curve_q4_7, label='b50000_lr0.005')
+# plt.plot(curve_q4_8, label='b50000_lr0.01')
+# plt.plot(curve_q4_9, label='b50000_lr0.02')
+# plt.legend()
+# plt.show()
+
+################ q4 #########################
 plt.figure()
-plt.plot(curve_q4_1, label='b10000_lr0.005')
-plt.plot(curve_q4_2, label='b10000_lr0.01')
-plt.plot(curve_q4_3, label='b10000_lr0.02')
-plt.plot(curve_q4_4, label='b30000_lr0.005')
-plt.plot(curve_q4_5, label='b30000_lr0.01')
-plt.plot(curve_q4_6, label='b30000_lr0.02')
-plt.plot(curve_q4_7, label='b50000_lr0.005')
-plt.plot(curve_q4_8, label='b50000_lr0.01')
-plt.plot(curve_q4_9, label='b50000_lr0.02')
+plt.plot(curve_q4_run_1, label='none')
+plt.plot(curve_q4_run_2, label='rtg')
+plt.plot(curve_q4_run_3, label='baseline')
+plt.plot(curve_q4_run_4, label='rtg and baseline')
 plt.legend()
 plt.show()
 
