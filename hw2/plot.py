bef75d3adc711a326fd13e193397f4e301a9720c
Haoran Liao <haoran@physics.mcgill.ca>
2020-09-24 13:19:16 -0700

    data

--- hw2/plot.py
+++ hw2/plot.py
@@ -8,11 +8,11 @@ curve_lb1 = np.load("/Users/haoran/Documents/GitHub/homework_fall2020/hw2/data/q
 curve_lb2 = np.load("/Users/haoran/Documents/GitHub/homework_fall2020/hw2/data/q1_lb_rtg_dsa.npy")
 curve_lb3 = np.load("/Users/haoran/Documents/GitHub/homework_fall2020/hw2/data/q1_lb_rtg_na.npy")
 
-# curve_q2_1 = np.load("/Users/haoran/Documents/GitHub/homework_fall2020/hw2/data/q2_b2000_r0.1.npy")
-# curve_q2_2 = np.load("/Users/haoran/Documents/GitHub/homework_fall2020/hw2/data/q2_b3000_r0.05.npy")
-# curve_q2_3 = np.load("/Users/haoran/Documents/GitHub/homework_fall2020/hw2/data/q2_b4000_r0.1.npy")
+curve_q2_1 = np.load("/Users/haoran/Documents/GitHub/homework_fall2020/hw2/data/q2_b3000_r0.01.npy")
+curve_q2_2 = np.load("/Users/haoran/Documents/GitHub/homework_fall2020/hw2/data/q2_b4000_r0.008.npy")
+curve_q2_3 = np.load("/Users/haoran/Documents/GitHub/homework_fall2020/hw2/data/q2_b5000_r0.008.npy")
 
-curve_q3 = np.load("/Users/haoran/Documents/GitHub/homework_fall2020/hw2/data/q2_b3000_r0.01.npy")
+# curve_q3 = np.load("")
 
 curve_q4_1 = np.load("/Users/haoran/Documents/GitHub/homework_fall2020/hw2/data/q4_search_b10000_lr0.005_rtg_nnbaseline.npy")
 curve_q4_2 = np.load("/Users/haoran/Documents/GitHub/homework_fall2020/hw2/data/q4_search_b10000_lr0.01_rtg_nnbaseline.npy")
@@ -45,31 +45,31 @@ from matplotlib import pyplot as plt
 
 ################## q2 #################
 # plt.figure()
-# plt.plot(curve_q2_1, label='b2000_r0.1')
-# plt.plot(curve_q2_2, label='b3000_r0.05')
-# plt.plot(curve_q2_3, label='b4000_r0.1')
+# plt.plot(curve_q2_1, label='b3000_r0.01')
+# plt.plot(curve_q2_2, label='b4000_r0.008')
+# plt.plot(curve_q2_3, label='b5000_r0.008')
 # plt.legend()
 # plt.show()
 
 
 ################# q3 #################
-plt.figure()
-plt.plot(curve_q3, label='q3')
-plt.legend()
-plt.show()
+# plt.figure()
+# plt.plot(curve_q3, label='q3')
+# plt.legend()
+# plt.show()
 
 
 ################# q4 ##################
-# plt.figure()
-# plt.plot(curve_q4_1, label='b10000_lr0.005')
-# plt.plot(curve_q4_2, label='b10000_lr0.01')
-# plt.plot(curve_q4_3, label='b10000_lr0.02')
-# plt.plot(curve_q4_4, label='b30000_lr0.005')
-# plt.plot(curve_q4_5, label='b30000_lr0.01')
-# plt.plot(curve_q4_6, label='b30000_lr0.02')
-# plt.plot(curve_q4_7, label='b50000_lr0.005')
-# plt.plot(curve_q4_8, label='b50000_lr0.01')
-# plt.plot(curve_q4_9, label='b50000_lr0.02')
-# plt.legend()
-# plt.show()
+plt.figure()
+plt.plot(curve_q4_1, label='b10000_lr0.005')
+plt.plot(curve_q4_2, label='b10000_lr0.01')
+plt.plot(curve_q4_3, label='b10000_lr0.02')
+plt.plot(curve_q4_4, label='b30000_lr0.005')
+plt.plot(curve_q4_5, label='b30000_lr0.01')
+plt.plot(curve_q4_6, label='b30000_lr0.02')
+plt.plot(curve_q4_7, label='b50000_lr0.005')
+plt.plot(curve_q4_8, label='b50000_lr0.01')
+plt.plot(curve_q4_9, label='b50000_lr0.02')
+plt.legend()
+plt.show()
 
