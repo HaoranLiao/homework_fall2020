31c8448ea756cfac94dbdcc13418e6a1077448b6
Haoran Liao <haoran@physics.mcgill.ca>
2020-09-24 22:59:50 -0700

    plots

--- hw2/plot.py
+++ hw2/plot.py
@@ -12,7 +12,7 @@ curve_q2_1 = np.load("/Users/haoran/Documents/GitHub/homework_fall2020/hw2/data/
 curve_q2_2 = np.load("/Users/haoran/Documents/GitHub/homework_fall2020/hw2/data/q2_b4000_r0.008.npy")
 curve_q2_3 = np.load("/Users/haoran/Documents/GitHub/homework_fall2020/hw2/data/q2_b5000_r0.008.npy")
 
-# curve_q3 = np.load("")
+curve_q3 = np.load("/Users/haoran/Documents/GitHub/homework_fall2020/hw2/data/q3_b40000_r0.005_2.npy")
 
 curve_q4_1 = np.load("/Users/haoran/Documents/GitHub/homework_fall2020/hw2/data/q4_search_b10000_lr0.005_rtg_nnbaseline.npy")
 curve_q4_2 = np.load("/Users/haoran/Documents/GitHub/homework_fall2020/hw2/data/q4_search_b10000_lr0.01_rtg_nnbaseline.npy")
@@ -38,6 +38,8 @@ from matplotlib import pyplot as plt
 # plt.plot(curve_sb2, label='q1_sb_rtg_dsa')
 # plt.plot(curve_sb3, label='q1_sb_rtg_na')
 # plt.legend()
+# plt.xlabel("Iterations")
+# plt.ylabel("Average Evaluation Returns")
 # plt.show()
 
 # plt.figure()
@@ -46,14 +48,18 @@ from matplotlib import pyplot as plt
 # plt.plot(curve_lb2, label='q1_lb_rtg_dsa')
 # plt.plot(curve_lb3, label='q1_lb_rtg_na')
 # plt.legend()
+# plt.xlabel("Iterations")
+# plt.ylabel("Average Evaluation Returns")
 # plt.show()
 
 ################## q2 #################
 # plt.figure()
-# plt.plot(curve_q2_1, label='b3000_r0.01')
-# plt.plot(curve_q2_2, label='b4000_r0.008')
+# # plt.plot(curve_q2_1, label='b3000_r0.01')
+# # plt.plot(curve_q2_2, label='b4000_r0.008')
 # plt.plot(curve_q2_3, label='b5000_r0.008')
 # plt.legend()
+# plt.xlabel("Iterations")
+# plt.ylabel("Average Evaluation Returns")
 # plt.show()
 
 
@@ -61,6 +67,8 @@ from matplotlib import pyplot as plt
 # plt.figure()
 # plt.plot(curve_q3, label='q3')
 # plt.legend()
+# plt.xlabel("Iterations")
+# plt.ylabel("Average Evaluation Returns")
 # plt.show()
 
 
@@ -76,6 +84,8 @@ from matplotlib import pyplot as plt
 # plt.plot(curve_q4_8, label='b50000_lr0.01')
 # plt.plot(curve_q4_9, label='b50000_lr0.02')
 # plt.legend()
+# plt.xlabel("Iterations")
+# plt.ylabel("Average Evaluation Returns")
 # plt.show()
 
 ################ q4 #########################
@@ -85,5 +95,7 @@ plt.plot(curve_q4_run_2, label='rtg')
 plt.plot(curve_q4_run_3, label='baseline')
 plt.plot(curve_q4_run_4, label='rtg and baseline')
 plt.legend()
+plt.xlabel("Iterations")
+plt.ylabel("Average Evaluation Returns")
 plt.show()
 
